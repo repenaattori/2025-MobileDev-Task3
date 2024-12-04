f441931e6cb9c0150aece5e3cd15cdb59f3c6527
Reima Riihimäki <reimarii@oamk.fi>
2024-12-04 19:41:30 +0200

    Testing added

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} testID='view'>
+      <Text style={styles.header} testID='header'>Personal information</Text>
+      <TextInput style={styles.textInput} placeholder='First name'/>
+      <TextInput style={styles.textInput} placeholder='Last name' />
+      <TouchableOpacity onPress={() => console.log("Submit")}>
+        <Text style={{backgroundColor: '#F09E51',textAlign: 'center', borderWidth: 1, padding: 10, fontWeight: 'bold'}}>SUBMIT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 40,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#70675E', 
   },
+  header:{
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#ffffff', 
+    marginBottom: 10
+  },
+  textInput:{
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    marginBottom: 10
+  }
+
 });
